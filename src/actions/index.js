7fd699dce776ea6c71def7c51345d0a1ca47549f
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-06-16 17:37:26 +0530

    setup api request

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// import axios from 'axios';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts  = () => {
+
+	const promise = jsonPlaceholder.get("/posts");
+
+	return({
+		type : 'FETCH_POSTS',
+		payload : promise
+	});
+};
