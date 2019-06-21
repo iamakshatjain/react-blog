81fba3192fdca857dee6ff908a702e1d2f99f41c
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-06-22 00:59:50 +0530

    apply memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // import axios from 'axios';
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts  = () => async dispatch => {
@@ -10,10 +11,20 @@ export const fetchPosts  = () => async dispatch => {
 	};
 //since here there is only one line to the fetchPost action creater we return directly the function that we have.
 
-export const fetchUser = (id) => async dispatch => {
+// export const fetchUser = (id) => async dispatch => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+// 	dispatch({
+// 		type : 'FETCH_USER',
+// 		payload : response.data
+// 	});
+// }
+
+export const fetchUser = (id) => dispatch => _fetchUser(id,dispatch);//the fetch user function returns a function that is called with dispatch
+
+const _fetchUser = _.memoize( async (id,dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 	dispatch({
 		type : 'FETCH_USER',
 		payload : response.data
 	});
-}
+});
