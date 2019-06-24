ab20f25d7fd6ce7880d7c1214d81f17bb2156756
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-06-25 00:31:56 +0530

    refactor code

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,12 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPostsAndUsers = () => async (dispatch,getState) => {
 	await dispatch(fetchPosts());//we would wait for posts data
-	// console.log(getState().posts);
-	const userIds = _.uniq(_.map(getState().posts,'userId'));
-	userIds.forEach(id => dispatch(fetchUser(id)));//we won't wait for the user data
+
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach(id => dispatch(fetchUser(id)))//we are not waiting for user data here.
+		.value();
 }
 
 
